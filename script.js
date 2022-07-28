a0536cec45937b0af18b226d23829edb9468a544
Zeerah <109766347+Zeerah@users.noreply.github.com>
2022-07-28 15:50:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const change = document.getElementById("Image 1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("Image 2");
+
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change3 = document.getElementById("Image 3");
+
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change4 = document.getElementById("Image 4");
+
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change5 = document.getElementById("Image 5");
+
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("Image 6");
+
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change7 = document.getElementById("Image 7");
+
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change8 = document.getElementById("Image 8");
+
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+
+
+
+
+
+
+
+
+
